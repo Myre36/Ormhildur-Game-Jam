4da25533bf6ea2544db8ebcaef1d80aed190ed90
Gab <gamajorgensen@outlook.com>
2025-10-08 12:04:10 +0200

    Added doggie

--- Assets/_Scripts/Animal Behaviour/AnimalMovement.cs
+++ Assets/_Scripts/Animal Behaviour/AnimalMovement.cs
@@ -1,63 +1,61 @@
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UIElements;
 
 public class AnimalMovement : MonoBehaviour
 {
     [SerializeField]
+    private GameObject sprite;
+    [SerializeField]
     private float maxDistanceVertical = 10f;
     [SerializeField]
     private float maxDistanceHorizontal = 10f;
 
-    [SerializeField]
-    private float moveSpeed;
-    private float timeTillDest;
-
     private float timeBetweenMovements = 5f;
     private float currentTimeBetween;
-    private float step;
 
     [SerializeField]
-    private Rigidbody2D rb;
+    private NavMeshAgent agent;
 
-    private Vector2 newPosition;
+    [SerializeField]
+    private Animator animator;
+
+    private Vector3 newPosition;
 
     [SerializeField]
     private GameObject playerChar;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentTimeBetween = Time.realtimeSinceStartup + timeBetweenMovements;
         playerChar = GameObject.Find("Player");
+        agent.updateRotation = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, playerChar.transform.position) < 1f) {
+        if (Vector3.Distance(transform.position, playerChar.transform.position) < 1f) {
+            animator.SetBool("IsWalking", false);
             return;
         }
-        else {
-            if (currentTimeBetween < Time.realtimeSinceStartup) {
-                Reposition();
-            }
-
-            if (Vector2.Distance(transform.position, newPosition) > 0.1f) {
-                transform.position = Vector2.MoveTowards(transform.position, newPosition, timeTillDest);
-            }
-            //transform.position = Vector2.Lerp (transform.position, newPosition, timeTillDest * Time.deltaTime);
+        
+        if (currentTimeBetween < Time.realtimeSinceStartup) {
+            Reposition();
+            Debug.Log("Repositioning");
         }
+
+        agent.SetDestination(newPosition);
+        if (Vector3.Distance(newPosition, transform.position) < 0.1f) animator.SetBool("IsWalking", false );
     }
 
     void Reposition() {
-        
-        newPosition = new Vector2
-            (Random.Range(maxDistanceVertical, -maxDistanceVertical), Random.Range(maxDistanceHorizontal, -maxDistanceHorizontal));
+        Debug.Log("Finding New Position");
+        newPosition = new Vector3
+            (Random.Range(maxDistanceVertical, -maxDistanceVertical), transform.position.y, Random.Range(maxDistanceHorizontal, -maxDistanceHorizontal));
         Debug.Log(newPosition);
-        timeTillDest = 0f;
-        timeTillDest = Vector3.Distance(transform.position, newPosition) * Time.deltaTime;
-        if (newPosition.x < transform.position.x) { gameObject.transform.rotation = Quaternion.Euler(transform.rotation.x, 180f, transform.rotation.z); }
-        else gameObject.transform.rotation = Quaternion.Euler(transform.rotation.x, 0f, transform.rotation.z);
-            currentTimeBetween = Time.realtimeSinceStartup + timeBetweenMovements;
+        animator.SetBool("IsWalking", true);
+        currentTimeBetween = Time.realtimeSinceStartup + timeBetweenMovements;
+        if (newPosition.x < transform.position.x) { sprite.transform.rotation = Quaternion.Euler(-30f, 180f, sprite.transform.rotation.z); }
+        else sprite.transform.rotation = Quaternion.Euler(30f, 0f, sprite.transform.rotation.z);
     }
 }
---
+++ Assets/_Scripts/AnimalInteraction.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalInteraction : MonoBehaviour
+{
+    [SerializeField]
+    private MoodController animal;
+
+    [SerializeField]
+    private GameObject animalMenu;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+           if (Vector3.Distance(transform.position, animal.transform.position) < 1f) {
+                Debug.Log("Menu Open");
+                animalMenu.SetActive(true);
+           }
+           else { Debug.Log("Too Far"); }
+           
+        }
+        if (Vector3.Distance(transform.position, animal.transform.position) > 1f) {
+            animalMenu.SetActive(false);
+        }
+    }
+}
