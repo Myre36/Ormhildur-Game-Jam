9908507f0d8f8938f73b4414f81c7e4a81fe57fb
Maya <mayavenas@gmail.com>
2025-10-08 10:33:29 +0200

    tmp + slay

--- Assets/_Scripts/MoodBoard/MoodController.cs
+++ Assets/_Scripts/MoodBoard/MoodController.cs
@@ -1,16 +1,76 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoodController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Slider hungerBar;
+    public Slider happinessBar;
+    public Slider hygieneBar;
+
+    public float hunger = 100f;
+    public float happiness = 100f;
+    public float hygiene = 100f;
+
+    public float decreaseRate = 0.1f;
+    public float feedBoost = 50f;
+    public float hygieneBoost = 30f;
+    public float happinessPenaltyFromBath = 10f;
+    private void Start()
     {
-        
+        UpdateBars();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        hunger -= (decreaseRate / 6f) * Time.deltaTime;
+        happiness -= (decreaseRate / 6f) * Time.deltaTime;
+        hygiene -= (decreaseRate / 6f) * Time.deltaTime;
+
+
+        hunger = Mathf.Clamp(hunger, 0f, 100f);
+        happiness = Mathf.Clamp(happiness, 0f, 100f);
+        hygiene = Mathf.Clamp(hygiene, 0f, 100f);
+
+        UpdateBars();
+    }
+
+    public void Feed()
+    {
+        hunger = 100f;
+        happiness += feedBoost / 2f;
+
+        happiness = Mathf.Clamp(happiness, 0f, 100f);
+        UpdateBars();
+    }
+    public void Bathe()
+    {
+        hygiene += hygieneBoost;
+        happiness -= happinessPenaltyFromBath;
+
+        hygiene = Mathf.Clamp(hygiene, 0f, 100f);
+        happiness = Mathf.Clamp(happiness, 0f, 100f);
+
+        UpdateBars();
+    }
+
+    public void Play()
+    {
+        happiness = 100f;
+  
+        UpdateBars();
+    }
+    public void Pet()
+    {
+        happiness = 100f;
+
+        UpdateBars();
+    }
+
+    private void UpdateBars()
+    {
+        if (hungerBar) hungerBar.value = hunger / 100f;
+        if (happinessBar) happinessBar.value = happiness / 100f;
+        if (hygieneBar) hygieneBar.value = hygiene / 100f;
     }
 }
