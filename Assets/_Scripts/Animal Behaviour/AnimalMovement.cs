33812d8d5da2589915fe988811304e20fb461ed4
Gab <gamajorgensen@outlook.com>
2025-10-08 10:07:15 +0200

    Animal Movement Script
    Meow

--- Assets/_Scripts/Animal Behaviour/AnimalMovement.cs
+++ Assets/_Scripts/Animal Behaviour/AnimalMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class AnimalMovement : MonoBehaviour
 {
@@ -9,39 +10,54 @@ public class AnimalMovement : MonoBehaviour
 
     [SerializeField]
     private float moveSpeed;
+    private float timeTillDest;
 
     private float timeBetweenMovements = 5f;
     private float currentTimeBetween;
     private float step;
 
+    [SerializeField]
+    private Rigidbody2D rb;
+
     private Vector2 newPosition;
+
+    [SerializeField]
+    private GameObject playerChar;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       transform.position = new Vector2 
-            (Random.Range(maxDistanceVertical, -maxDistanceVertical), Random.Range(maxDistanceHorizontal, -maxDistanceHorizontal));
-
         currentTimeBetween = Time.realtimeSinceStartup + timeBetweenMovements;
+        playerChar = GameObject.Find("Player");
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float deltaTime = Time.deltaTime;
-        step += moveSpeed * deltaTime;
-        if (currentTimeBetween < Time.realtimeSinceStartup) {
-            Reposition();
+        if (Vector2.Distance(transform.position, playerChar.transform.position) < 1f) {
+            return;
         }
-    
-        transform.position = Vector2.Lerp (transform.position, newPosition, step);
+        else {
+            if (currentTimeBetween < Time.realtimeSinceStartup) {
+                Reposition();
+            }
+
+            if (Vector2.Distance(transform.position, newPosition) > 0.1f) {
+                transform.position = Vector2.MoveTowards(transform.position, newPosition, timeTillDest);
+            }
+            //transform.position = Vector2.Lerp (transform.position, newPosition, timeTillDest * Time.deltaTime);
+        }
     }
 
     void Reposition() {
-        step = 0f;
+        
         newPosition = new Vector2
             (Random.Range(maxDistanceVertical, -maxDistanceVertical), Random.Range(maxDistanceHorizontal, -maxDistanceHorizontal));
         Debug.Log(newPosition);
-        currentTimeBetween = Time.realtimeSinceStartup + timeBetweenMovements;
+        timeTillDest = 0f;
+        timeTillDest = Vector3.Distance(transform.position, newPosition) * Time.deltaTime;
+        if (newPosition.x < transform.position.x) { gameObject.transform.rotation = Quaternion.Euler(transform.rotation.x, 180f, transform.rotation.z); }
+        else gameObject.transform.rotation = Quaternion.Euler(transform.rotation.x, 0f, transform.rotation.z);
+            currentTimeBetween = Time.realtimeSinceStartup + timeBetweenMovements;
     }
 }
