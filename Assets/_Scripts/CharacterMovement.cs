7ce0c11a54ff441af0c499b5ef43134bcceee0ba
Myre36 <67600948+Myre36@users.noreply.github.com>
2025-10-08 14:46:19 +0200

    Something in movement apparently

--- Assets/_Scripts/CharacterMovement.cs
+++ Assets/_Scripts/CharacterMovement.cs
@@ -19,12 +19,6 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField]
     private SpriteRenderer sprite;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
