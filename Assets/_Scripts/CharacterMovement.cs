1fcf346cadc00a9615eb66d072bf2e99e083a698
Myre36 <67600948+Myre36@users.noreply.github.com>
2025-10-08 10:30:17 +0200

    Added stuff

--- Assets/_Scripts/CharacterMovement.cs
+++ Assets/_Scripts/CharacterMovement.cs
@@ -7,6 +7,12 @@ public class CharacterMovement : MonoBehaviour
 
     private Vector2 moveDirection;
 
+    [SerializeField]
+    private Animator anim;
+
+    [SerializeField]
+    private SpriteRenderer sprite;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,5 +29,20 @@ public class CharacterMovement : MonoBehaviour
 
         Vector3 newPosition = transform.position + movement * Time.deltaTime;
         this.transform.position = newPosition;
+
+        if(moveDirection.x > 0f && moveDirection.y > 0f)
+        {
+            anim.SetBool("WalkingUp", false);
+            anim.SetBool("WalkingDown", false);
+            anim.SetBool("WalkingSide", false);
+            anim.SetBool("RunningUp", false);
+            anim.SetBool("RunningDown", false);
+            anim.SetBool("RunningSide", false);
+            anim.SetBool("RunningDiagonal", false);
+
+            anim.SetBool("WalkingDiagonal", true);
+
+            sprite.flipX = false;
+        }
     }
 }
